85de72ad20dc1a8dc9d3b456086e4c6fb525bd2c
arif <muhdarifrawi@gmail.com>
2022-07-06 12:42:00 +0800

    Made changes to validation: - validation will be done in another file. - validation will be done input by input instead of mass checks. - this will give better control of what is wanted in the particular input.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+
 async function main(){
     const db = await MongoUtil.connect(MONGO_URI, "stalls")
     console.log("connected to database")
@@ -20,23 +21,13 @@ async function main(){
     })
 
     app.post("/stalls",async function(req,res,next){
-        // using regex to check if characters are in english
-        var english = /^[A-Za-z0-9]*$/;
+        
         console.log(req.body)
-        let stallName = req.body.stallName
+        // let stallName = req.body.stallName
 
-        let infoArr = [stallName]
-        // validate.check(infoArr)
-        // inverse the results. if non-english is caught, it turns false to true
-        let checkEnglish = infoArr.filter(info => !english.test(info))
-        validate
-        if (checkEnglish.length >= 1){
-            console.log("Must be english characters")
-            res.sendStatus(400)
-        }
-        else{
-            res.sendStatus(201)
-        }
+        // let infoArr = [stallName]
+        validate.check(res,req.body)
+        
     })
 
 }
--- js/validation.js
+++ js/validation.js
@@ -1,8 +1,39 @@
-function check(arr){
+let errors = {}
 
-    console.log("validation run")
-
+function check(res,req){
+    checkStallName(req)
+    checkStallType(req)
+    if(Object.keys(errors).length != 0){
+        res.status(400).send(errors)
+    }
+    else{
+        res.sendStatus(200)
+    }
 }
+
+function checkStallName(req){
+    console.log(req)
+    let stallName = req.stallName
+    if(stallName.trim().length === 0 ){
+        errors["stallName"] = "Cannot be left empty."
+    }
+    if(stallName.trim().length > 25 ){
+        errors["stallName"] = "Name is too long."
+    }
+}
+
+function checkStallType(req){
+    console.log(req)
+    let stallType = req.stallType
+    if(stallType.trim().length === 0 ){
+        errors["stallType"] = "Cannot be left empty."
+    }
+    if(stallType.trim().length > 25 ){
+        errors["stallType"] = "Name is too long."
+    }
+}
+
+
 
 module.exports = {
     check
