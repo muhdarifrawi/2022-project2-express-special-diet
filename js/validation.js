b681647bced2627a374d4d32ecd1c7d0dd9df7b1
arif <muhdarifrawi@gmail.com>
2022-07-07 12:40:15 +0800

    added more validation.

--- js/validation.js
+++ js/validation.js
@@ -1,39 +1,138 @@
 let errors = {}
 
-function check(res,req){
+function check(res, req) {
     checkStallName(req)
     checkStallType(req)
-    if(Object.keys(errors).length != 0){
+    checkLocation(req)
+    checkFoodType(req)
+    checkCuisine(req)
+    checkPriceRange(req)
+    checkOpeningHours(req)
+    if (Object.keys(errors).length != 0) {
         res.status(400).send(errors)
     }
-    else{
+    else {
         res.sendStatus(200)
     }
 }
 
-function checkStallName(req){
-    console.log(req)
+function checkStallName(req) {
     let stallName = req.stallName
-    if(stallName.trim().length === 0 ){
-        errors["stallName"] = "Cannot be left empty."
+    if (stallName.trim().length === 0) {
+        errors["stallName"] = "Stall name cannot be left empty."
     }
-    if(stallName.trim().length > 25 ){
-        errors["stallName"] = "Name is too long."
+    if (stallName.trim().length > 25) {
+        errors["stallName"] = "Stall name entry is too long."
     }
 }
 
-function checkStallType(req){
-    console.log(req)
+function checkStallType(req) {
     let stallType = req.stallType
-    if(stallType.trim().length === 0 ){
-        errors["stallType"] = "Cannot be left empty."
+    if (stallType.trim().length === 0) {
+        errors["stallType"] = "Stall type cannot be left empty."
     }
-    if(stallType.trim().length > 25 ){
-        errors["stallType"] = "Name is too long."
+    if (stallType.trim().length > 25) {
+        errors["stallType"] = "Stall type entry is too long."
     }
 }
 
+function checkLocation(req) {
+    let location = req.location
+    console.log(location)
+    // BUILDING NAME
+    if (location.buildingName.trim().length === 0) {
+        errors["buildingName"] = "Building name cannot be left empty"
+    }
+    if (location.buildingName.trim().length > 25) {
+        errors["buildingName"] = "Building name entry is too long."
+    }
+    // ADDRESS
+    if (location.address.trim().length === 0) {
+        errors["address"] = "Address cannot be left empty"
+    }
+    if (location.address.trim().length > 25) {
+        errors["address"] = "Address entry is too long."
+    }
+    // STALL NUMBER
+    if (location.stallNumber.trim().length === 0) {
+        errors["stallNumber"] = "Stall number cannot be left empty"
+    }
+    if (location.stallNumber.trim().length > 8) {
+        errors["stallNumber"] = "Stall number entry is too long."
+    }
+    if (location.stallNumber.trim().length > 8) {
+        errors["stallNumber"] = "Stall number entry is too long."
+    }
+    let stallNumber = location.stallNumber.trim()
+    stallNumber = stallNumber.replaceAll("-", "")
+    if (!isAlphaNumeric(stallNumber)){
+        errors["stallNumber"] = "Stall number must be alphanumeric."
+    }
 
+}
+
+function checkFoodType(req){
+    let foodType = req.foodType
+    if(foodType.length == 0){
+        errors["foodType"] = "Food type should have one selection minimally." 
+    }
+}
+
+function checkCuisine(req){
+    if (req.cuisine.trim().length === 0) {
+        errors["cuisine"] = "Cuisine cannot be left empty"
+    }
+    if (req.cuisine.trim().length > 25) {
+        errors["cuisine"] = "Cuisine entry is too long."
+    }
+}
+
+function checkPriceRange(req){
+    if(!isNumeric(req.priceRange[0]) || !isNumeric(req.priceRange[1])){
+        errors["priceRange"] = "Price range must be numeric."
+    }
+    if(req.priceRange[0] >= req.priceRange[1]){
+        errors["priceRange"] = "Minimum price cannot be larger than maximum price."
+    }
+}
+
+function checkImages(req){
+    if(req.images != ""){
+        
+    }
+}
+
+function checkOpeningHours(req){
+    let isUnsure = req.openingHours.map(({day, unsure}) => ({[day]:unsure}) )
+    console.log(isUnsure)
+}
+
+function isAlphaNumeric(str) {
+    for (let i = 0, len = str.length; i < len; i++) {
+        charCode = str.charCodeAt(i);
+        if  (
+            !(charCode > 47 && charCode < 58) && // (0 - 9)
+            !(charCode > 64 && charCode < 91) && // (A - Z)
+            !(charCode > 96 && charCode < 123)   // (a - z)
+            ) 
+        { 
+            return false;
+        }
+    }
+    return true;
+};
+function isNumeric(str) {
+    for (let i = 0, len = str.length; i < len; i++) {
+        charCode = str.charCodeAt(i);
+        if  (
+            !(charCode > 47 && charCode < 58)// (0 - 9)
+            ) 
+        { 
+            return false;
+        }
+    }
+    return true;
+};
 
 module.exports = {
     check
